9ecc60cfebee5f28b70d9a518fedbf0dde3d5a23
anubhootipandey <anubhootipandey05@gmail.com>
2024-04-13 11:54:44 +0530

    added search functionality

--- src/components/Listing.js
+++ src/components/Listing.js
@@ -3,93 +3,106 @@ import './Listing.css';
 import axios from 'axios';
 
 const Page = () => {
-  const[product, setProduct] = useState([]);
-  const[category, setCategory] = useState([]);
+  const [state, setState] = useState({
+    products: [],
+    categories: [],
+    searchInput: '',
+    filteredProducts: []
+  });
 
   const filterFunction = (item) => {
-    axios.get("https://fakestoreapi.com/products/category/" + item).then((response) => {
-      setProduct(response.data);
-    });
+    axios.get("https://fakestoreapi.com/products/category/" + item)
+      .then((response) => {
+        setState(prevState => ({
+          ...prevState,
+          products: response.data,
+          filteredProducts: response.data
+        }));
+      });
   }
-               
+
   useEffect(() => {
-    axios.get("https://fakestoreapi.com/products").then((response) => {
-      setProduct(response.data);
-    });
-    axios.get("https://fakestoreapi.com/products/categories").then((response) => {
-      setCategory(response.data);
-    });
+    axios.get("https://fakestoreapi.com/products")
+      .then((response) => {
+        setState(prevState => ({
+          ...prevState,
+          products: response.data,
+          filteredProducts: response.data
+        }));
+      });
+    axios.get("https://fakestoreapi.com/products/categories")
+      .then((response) => {
+        setState(prevState => ({
+          ...prevState,
+          categories: response.data
+        }));
+      });
   }, []);
+
+  const handleSearchInput = (e) => {
+    const value = e.target.value;
+    setState(prevState => ({
+      ...prevState,
+      searchInput: value,
+      filteredProducts: value === '' ? state.products : state.products.filter(product =>
+        product.title.toLowerCase().includes(value.toLowerCase())
+      )
+    }));
+  }
+
   return (
     <>
-    <nav className='main-nav'>
-    <div className="sidenav">
-      <div className="logo">
-        <a href="#"></a>
-      </div>
-      <ul className="mainNav">
-        <li className="single-nav" onClick={() => filterFunction(category[0])}>
-          <a href="#">
-            <i className=""></i>
-            <span className='text' >{category[0]}</span>
-          </a>
-        </li>
-        <li className="single-nav" onClick={() => filterFunction(category[1])}>
-          <a href="#">
-            <i className=""></i>
-            <span className='text'>{category[1]}</span>
-          </a>
-        </li>
-        <li className="single-nav" onClick={() => filterFunction(category[2])}>
-          <a href="#">
-            <i className=""></i>
-            <span className='text'>{category[2]}</span>
-          </a>
-        </li>
-        <li className="single-nav" onClick={() => filterFunction(category[3])}>
-          <a href="#">
-            <i className=""></i>
-            <span className='text'>{category[3]}</span>
-          </a>
-        </li>
-      </ul>
-    </div>
+      <nav className='main-nav'>
+        <div className="sidenav">
+          <div className="logo">
+            <a href="#"></a>
+          </div>
+          <ul className="mainNav">
+            {state.categories.map((category, index) => (
+              <li key={index} className="single-nav" onClick={() => filterFunction(category)}>
+                <a href="#">
+                  <span className='text'>{category}</span>
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
 
         <div className='logo'>
-            <h2>
-                <span>online</span>
-                <span>store</span>
-            </h2>
+          <h2>
+            <span>online</span>
+            <span>store</span>
+          </h2>
         </div>
         <div className='search-box'>
-          <input type="text" 
-          placeholder="Search Products"></input>
+          <input
+            type="text"
+            placeholder="Search Products"
+            value={state.searchInput}
+            onChange={handleSearchInput}
+          />
           <button type="button" className='search-btn'>Search</button>
         </div>
-    </nav>
-    <div className="main">
-    {product.map((item) => {
-                return(
-                    
-                    <div className="cards">
-                      <div className="image">
-                        <img src={item.image} alt="" />
-                      </div>
-                      <div className="title">
-                          <h1>{item.title}</h1>
-                      </div>
-                      <div className="price">
-                          <h3>${item.price}</h3>
-                      </div>
-                        <button className="cart-btn">Add to Cart</button>
-                        <button className="buy-btn">Buy Now</button>
-                    </div>
-                    
-                    );
-            })};
-    </div>
-
-  </>  )
+      </nav>
+      <div className="main">
+        {state.filteredProducts.map((item, index) => (
+          <div key={index} className="cards">
+            <div className="image">
+              <img src={item.image} alt="" />
+            </div>
+            <div className="title">
+              <h1>{item.title}</h1>
+            </div>
+            <div className="price">
+              <h3>${item.price}</h3>
+            </div>
+            <button className="cart-btn">Add to Cart</button>
+            <button className="buy-btn">Buy Now</button>
+          </div>
+        ))}
+      </div>
+    </>
+  )
 }
 
 export default Page;
