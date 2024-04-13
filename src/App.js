36e3565a90342baf5c25074130b840608a83ba7a
anubhootipandey <anubhootipandey05@gmail.com>
2024-04-13 11:48:15 +0530

    new changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React from 'react'
-import { Routes, Route} from "react-router-dom"
-import { Routes } from 'react-router-dom'
 import Listing from "./components/Listing"
 
 const App = () => {
